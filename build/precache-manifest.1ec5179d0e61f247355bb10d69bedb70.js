fd132ac311c8e0bbeb56379928ac4ddfa9c8b262
Mila <mila.windsor@gmail.com>
2020-07-05 11:24:33 -0400

    react deploy

---
+++ build/precache-manifest.1ec5179d0e61f247355bb10d69bedb70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b78bb7e105669276977e6840743c18c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "92dc19b869bc2941f41b",
+    "url": "/react-portfolio/static/css/main.cf635664.chunk.css"
+  },
+  {
+    "revision": "5e0d7f7fee62e3965732",
+    "url": "/react-portfolio/static/js/2.dbad9285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.dbad9285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92dc19b869bc2941f41b",
+    "url": "/react-portfolio/static/js/main.99923769.chunk.js"
+  },
+  {
+    "revision": "942191dcdba8c77365c9",
+    "url": "/react-portfolio/static/js/runtime-main.def5a4bc.js"
+  }
+]);
